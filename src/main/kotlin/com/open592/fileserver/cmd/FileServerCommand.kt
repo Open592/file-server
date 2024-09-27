ff002b56c491af790d1bf3917d2056ca7e332c1d
Open592 Developer <uss@open592.com>
2024-09-27 10:32:47 -0700

    cmd: Add kotlinx coroutines
    Signed-off-by: Open592 Developer <uss@open592.com>

--- src/main/kotlin/com/open592/fileserver/cmd/FileServerCommand.kt
+++ src/main/kotlin/com/open592/fileserver/cmd/FileServerCommand.kt
@@ -3,13 +3,12 @@ package com.open592.fileserver.cmd
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.main
 import com.github.michaelbull.logging.InlineLogger
+import kotlinx.coroutines.runBlocking
 
 fun main(args: Array<String>): Unit = FileServerCommand().main(args)
 
 class FileServerCommand : CliktCommand() {
-  override fun run() {
-    logger.info { "Hello world!" }
-  }
+  override fun run() = runBlocking { logger.info { "Hello world!" } }
 
   private companion object {
     private val logger = InlineLogger()
