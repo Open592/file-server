1e2049a55b8a3fd7d5923ab7771001f673c00476
Open592 Developer <uss@open592.com>
2024-09-29 00:47:05 -0700

    cmd: Remove logger
    Signed-off-by: Open592 Developer <uss@open592.com>

--- src/main/kotlin/com/open592/fileserver/cmd/Main.kt
+++ src/main/kotlin/com/open592/fileserver/cmd/Main.kt
@@ -1,13 +1,10 @@
 package com.open592.fileserver.cmd
 
-import com.github.michaelbull.logging.InlineLogger
 import com.google.inject.Guice
 import com.open592.fileserver.server.FileServer
 import com.open592.fileserver.server.FileServerModule
 
 object Main {
-  private val logger = InlineLogger()
-
   @JvmStatic
   fun main(args: Array<String>) {
     val fileServer = Guice.createInjector(FileServerModule).getInstance(FileServer::class.java)
